be6447fe2c9709530046d9b2b8e034998b1b7480
SHREYASHHH21 <shreyashlokhande94@gmail.com>
2025-06-21 02:10:11 +0530

    integrated Prisma client

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
         baseTheme: dark,
       }}
     >
-      <html lang="en" suppressHydrationWarning>
+      <html lang="en" suppressHydrationWarning={true}>
         <head>
           <link rel="icon" href="/logo.png" sizes="any" />
         </head>
@@ -35,11 +35,12 @@ export default function RootLayout({ children }) {
             <main className="min-h-screen">{children}</main>
             <Toaster richColors />
 
-            <footer className="bg-muted/50 py-12">
+            <footer className="bg-black py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made by Shreyash</p>
+                <p>Made by Code Blooded Lifeform</p>
               </div>
             </footer>
+
           </ThemeProvider>
         </body>
       </html>
